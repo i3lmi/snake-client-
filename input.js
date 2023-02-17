ae3447fdd1def2095a2a7ec651fb53226033c80c
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-16 19:43:48 -0500

    input.js

--- input.js
+++ input.js
@@ -103,10 +103,31 @@ console.log("Connecting ...");
 const conn = connect();
 setupInput();
 
-const { connect } = require("./client");
+// setup interface to handle user input from stdin
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+  
+  // handle user input
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  
+  module.exports = {
+    setupInput
+  };
+
+  const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
 setupInput();
