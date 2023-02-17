80001b07fa254f964a79434e922ce8525f856751
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-16 19:49:04 -0500

    input.js

--- input.js
+++ input.js
@@ -131,3 +131,62 @@ console.log("Connecting ...");
 const conn = connect();
 
 setupInput();
+
+// Stores the active TCP connection object.
+let connection;
+
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// handle user input
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {
+  setupInput
+};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+console.log('Connecting ...');
+const conn = connect();
+
+setupInput(conn);
+
+// handle user input
+const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up");
+    }
+  };
+
+  // handle user input
+const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    } else if (key === 'h') {
+      connection.write("Say: Hello");
+    }
+  };
+  
